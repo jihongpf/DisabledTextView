77b6e9004b51cc9b41b89fd94e292bec38aba950
Jihong Park <ggikko@hotmail.com>
2016-06-02 17:17:56 +0900

    init

---
+++ DisabledTextViewApp/app/src/main/java/ggikko/me/disabledtextviewapp/MainActivity.java
@@ -0,0 +1,14 @@
+package ggikko.me.disabledtextviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
